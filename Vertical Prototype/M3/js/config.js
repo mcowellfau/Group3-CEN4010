554f8d828cc5dc8b5e747f969b14a3a4e5f43e60
Yolanda-YV <yolanda.n.vega@gmail.com>
2023-07-14 16:57:26 -0400

    moved to new firebase project

---
+++ Vertical Prototype/M3/js/config.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBG8PGhE4UyVkhDtRoCFX3DFstBzBrnFjo",
+  authDomain: "nutriplay-b404b.firebaseapp.com",
+  projectId: "nutriplay-b404b",
+  storageBucket: "nutriplay-b404b.appspot.com",
+  messagingSenderId: "518981118424",
+  appId: "1:518981118424:web:15e3ef7cd14b47c24f6aad",
+  measurementId: "G-1QT3GFN7BX"
+};
+//Initialize firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+
