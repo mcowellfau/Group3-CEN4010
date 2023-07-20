e90545c5f9c744e8daa088702ca3aedaf1f5e91a
Yolanda-YV <yolanda.n.vega@gmail.com>
2023-07-20 16:15:44 -0400

    Added navbar user welcom

--- Dev/Test/js/member.js
+++ Dev/Test/js/member.js
@@ -2,12 +2,20 @@ $(document).ready(function(){
     auth.onAuthStateChanged((user) => {
         if (user) {
             //user is logged in
-            //Changing link in logo to home page instead of front page
+            console.log(user.email + " is logged in");
+            var userUID = user.uid;
             $("[href='index.html']").prop("href", "home.html");
-            console.log(user.email + " is logged in")
             $("#logoutBtn").removeClass("d-none");
             $(".member").removeClass("d-none");
             console.log("Login Fields hidden");
+            db.collection("user").doc(userUID).get().then((doc) => {
+                if (doc.exists) {
+                    console.log("User Data:", doc.data());
+                    var name = doc.data().name;
+                    $("#username>span").text("Hi " + name + "!");
+                }
+            })
+            
         } else {
             //no user is logged in or user signed out
             console.log("no user is logged in")
