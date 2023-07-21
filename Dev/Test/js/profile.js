1f4807d0684ea80fa08422a81cd7a11ed4636848
Yolanda-YV <yolanda.n.vega@gmail.com>
2023-07-20 22:48:02 -0400

    Added profile edit feature

--- Dev/Test/js/profile.js
+++ Dev/Test/js/profile.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    var userPersInfo = {
+        name: null,
+        dob: null,
+        bmr: null,
+        sex: null,
+        exp: null,
+    };
     auth.onAuthStateChanged((user) => {
         if (user) {
             const userUID = user.uid;
@@ -6,19 +13,75 @@ $(document).ready(function(){
             db.collection("user").doc(userUID).get().then((doc) => {
                 if (doc.exists) {
                     console.log("User Data:", doc.data());
-                    var name = doc.data().name;
-                    var dob = doc.data().dob;
-                    var bmr = doc.data().bmr;
-                    var sex = doc.data().sex;
-                    dob = new Date(dob.replaceAll("-", "\/"));
-                    $("#userDisplay").html("<td>" + name + "'s Profile</td>");
-                    $("#nameDisplay").html("<td>Name:</td>" + "<td>" + name + "</td>");
-                    $("#dobDisplay").html("<td>Date of Birth:</td>" + "<td>" + dob.toLocaleDateString() + "</td>");
-                    $("#bmrDisplay").html("<td>BMR:</td>" + "<td>" + bmr + " calories per day</td>");
-                    $("#sexDisplay").html("<td>Sex:</td>" + "<td>" + sex + "</td>");
+                    var userData = doc.data();
+                    displayUserInfo(userData);
+                    //Make edit profile fields default
+                    defaultEditFields(userData);
                 }
             })
+            //If save changes is pressed, update w/ new values
+            $("#editForm").on("submit", function(event){
+                event.preventDefault();
+                var name = $("#name").val();
+                var dob = $("#dob").val();
+                var bmr = $("#bmr").val();
+                var sex = $("#sex").find(":selected").val();
+                console.log(name, dob, bmr, sex);
+                //Storing user info into userPersInfo
+                userPersInfo.name = name;
+                userPersInfo.dob = dob;
+                userPersInfo.bmr = bmr;
+                userPersInfo.sex = sex;
+                userPersInfo.exp = 0;
+                console.log("User Info:", userPersInfo);
+
+                db.collection("user").doc(userUID).update({
+                    name: userPersInfo.name,
+                    dob: userPersInfo.dob,
+                    bmr: userPersInfo.bmr,
+                    sex: userPersInfo.sex,
+                    exp: userPersInfo.exp,
+                }).then(() => {
+                    console.log("Successfully updated user info");
+                    displayUserInfo(userPersInfo);
+                    //Hide modal when done
+                    $("#editProfileModal").modal("hide");
+                }).catch((error) => {
+                    console.log(error);
+                });
+            });
+            //cancel button
+            $("#cancelEdit").on("click", function() {
+                var userDoc = db.collection("user").doc(userUID);
+                userDoc.get().then((doc) => {
+                    if (doc.exists) {
+                        userData = doc.data();
+                        defaultEditFields(userData);
+                    }
+                })
+                //hide modal
+                $("#editProfileModal").modal("hide");
+            })
+            
         }
     });
 
+    function displayUserInfo(data) {
+        newdob = new Date(data.dob.replaceAll("-", "\/"));
+        $("#userDisplay>th").text(data.name + "'s Profile");
+        $("#nameDisplay>td:first").text("Name:");
+        $("#nameDisplay>td:last").text(data.name);
+        $("#dobDisplay>td:first").text("Date of Birth:");
+        $("#dobDisplay>td:last").text(newdob.toLocaleDateString());
+        $("#bmrDisplay>td:first").text("BMR");
+        $("#bmrDisplay>td:last").text(data.bmr);
+        $("#sexDisplay>td:first").text("Sex:");
+        $("#sexDisplay>td:last").text(data.sex);
+    }
+    function defaultEditFields(data) {
+        $("#name").val(data.name);
+        $("#dob").val(data.dob);
+        $("#bmr").val(data.bmr);
+        $("#sex").val(data.sex);
+    }
 })
